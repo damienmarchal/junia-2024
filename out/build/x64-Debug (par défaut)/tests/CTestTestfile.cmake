e70b55b2097d745b5906b768801bdbdcd7465af8
thomasraffray <thomas.raffray@student.junia.com>
2024-11-25 17:00:38 +0100

    upload projet

---
+++ out/build/x64-Debug (par défaut)/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrateur/source/junia-2024/tests
+# Build directory: C:/Users/Administrateur/source/junia-2024/out/build/x64-Debug (par défaut)/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/Administrateur/source/junia-2024/out/build/x64-Debug (par défaut)/tests/all-tests.exe[1]_include.cmake")
+subdirs("../_deps/googletest-build")
