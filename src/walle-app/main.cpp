af69cf9527e9d2811aeb5654b6e383c66842302a
thomasraffray <thomas.raffray@student.junia.com>
2024-12-02 11:35:58 +0100

    bug environnement

--- src/walle-app/main.cpp
+++ src/walle-app/main.cpp
@@ -9,8 +9,13 @@ int main(int argc, char** argv)
     // Créé en mémoire l'environment;
     Environment un_environment{-100,100,-100,100};
 
+    un_environment.left();
+
 	std::cout << "Hello world" << std::endl;
     std::cout << "Hello Thomas 123Soleil" << std::endl;
+
+    Robot mon_robot{&un_environment};
+    mon_robot.get_position_x();
 
     /// dt en seconde
     double dt = 0.01;
--- src/walle-lib/environnement.cpp
+++ src/walle-lib/environnement.cpp
@@ -4,3 +4,5 @@ Environment::Environment(double left, double right, double top, double bottom)
 {
 
 }
+
+double Environment::left() {return -1;}
