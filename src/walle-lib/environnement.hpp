a64e03d1b74ca5c905a8cca2f2416dfb8b84361d
Damien Marchal <damien.marchal@univ-lille1.fr>
2024-12-02 11:45:13 +0100

    Handle win32 declspec.

--- src/walle-lib/environnement.hpp
+++ src/walle-lib/environnement.hpp
@@ -2,8 +2,21 @@
 #include <cmath>
 #include <string>
 
+#ifdef __WIN32__
+#define DllExport __declspec( dllexport )
+#define DllImport __declspec( dllimport )
+#else
+#define DllExport
+#define DllImport
+#endif
 
-class __declspec( dllexport ) Environment
+#ifdef EXPORT
+#define EXPORT_NAME DllExport
+#else
+#define EXPORT_NAME DllImport
+#endif // EXPORT
+
+class EXPORT_NAME Environment
 {
 public:
     Environment(double left=-1, double right=-1, double top=-1, double bottom=1);
