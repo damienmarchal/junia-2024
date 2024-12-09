7065a6dea37e9985db789c0452c22f29975d94e1
Simon.Wavrant <wavrantsim@gmail.com>
2024-12-09 14:51:20 +0100

    Ajout de tests pour vérifier le constructeur de la classe Element et la méthode set_position.

--- tests/src/test_element.cpp
+++ tests/src/test_element.cpp
@@ -2,6 +2,20 @@
 #include <walle-lib/element.hpp>
 #include <walle-lib/environment.hpp>
 
+TEST(Element, test_constructor){
+    Environment e{0.0, 10.0, 0.0, 10.0};
+    Element element_1{&e};
+    Element element_2{&e, 1.0, 2.0};
+
+    std::vector<Element *> elements = e.get_elements_list();
+    EXPECT_EQ(elements.size(), 2);
+    EXPECT_EQ(elements[0], &element_1);
+    EXPECT_EQ(elements[1], &element_2);
+
+    elements[0]->set_position(3.0, 4.0);
+    EXPECT_NEAR(element_1.get_x(), 3.0, 0.00001);
+    EXPECT_NEAR(element_1.get_y(), 4.0, 0.00001);
+}
 
 TEST(Element, test_set_position){
     Environment e{0.0, 10.0, 0.0, 10.0};
