dce7cd05b8a30ae929ed509c5ee741d1c254cbaf
Simon.Wavrant <wavrantsim@gmail.com>
2024-12-09 14:46:03 +0100

    Ajout de tests pour vérifier les méthodes set_position et set_size dans la classe Element.

--- tests/src/test_element.cpp
+++ tests/src/test_element.cpp
@@ -1,2 +1,28 @@
 #include <gtest/gtest.h>
 #include <walle-lib/element.hpp>
+#include <walle-lib/environment.hpp>
+
+
+TEST(Element, test_set_position){
+    Environment e{0.0, 10.0, 0.0, 10.0};
+    Element element_1{&e, 0.0, 0.0};
+
+    element_1.set_position(1.0, 2.0);
+    EXPECT_NEAR(element_1.get_x(), 1.0, 0.00001);
+    EXPECT_NEAR(element_1.get_y(), 2.0, 0.00001);
+
+    element_1.set_position(3.0, 4.0);
+    EXPECT_NEAR(element_1.get_x(), 3.0, 0.00001);
+    EXPECT_NEAR(element_1.get_y(), 4.0, 0.00001);
+}
+
+TEST(Element, test_set_size){
+    Environment e{0.0, 10.0, 0.0, 10.0};
+    Element element_1{&e, 0.0, 0.0};
+
+    element_1.set_size(0.5);
+    EXPECT_NEAR(element_1.get_size(), 0.5, 0.00001);
+
+    element_1.set_size(0.7);
+    EXPECT_NEAR(element_1.get_size(), 0.7, 0.00001);
+}
